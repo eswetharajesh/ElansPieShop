67577cd6d67e1709616d82b0b19455e3346ef640
Swetha Elanchezhiyan <eswetharajesh@gmail.com>
2023-10-29 18:39:40 +0100

    Add controller

---
+++ ElansPieShop/Controllers/PieController.cs
@@ -0,0 +1,22 @@
+﻿using ElansPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElansPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+      private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)//constructor 
+        {
+            _pieRepository = pieRepository; 
+            _categoryRepository = categoryRepository;//constructor injection
+        }
+
+        public IActionResult List()
+        {
+            return View(_pieRepository.AllPies);
+        }
+    }
+}
